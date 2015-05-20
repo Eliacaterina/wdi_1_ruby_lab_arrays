81835aeeceeb4f98ed4a374e048f9929bf3dea9c
Your Name <you@example.com>
2015-05-20 10:29:07 +0800

    ruby_lab_arrays

--- lib/cube_numbers.rb
+++ lib/cube_numbers.rb
@@ -1,2 +1,6 @@
 def cube_numbers(numbers)
+	numbers.map do |i| 
+		i ** 3
+	end	
 end
+
--- lib/hashtags.rb
+++ lib/hashtags.rb
@@ -1,2 +1,10 @@
 def hashtags(tweet)
+  words = tweet.split
+  words.map do |word|
+    if word.length > 3
+     "#" + word
+    else
+     words.delete(word)
+    end
+	end
 end
